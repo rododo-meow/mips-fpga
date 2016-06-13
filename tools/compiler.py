b676af3f0dbefaa2dae63bde85b32c59946de824
rododo <lzn960520@126.com>
2016-06-13 09:36:32 +0800

    Add only output inst/data mif, add multi-file compilation test case

--- tools/compiler.py
+++ tools/compiler.py
@@ -8,18 +8,22 @@ def main():
     if len(sys.argv) < 6:
         print >>sys.stderr, "Usage: %s <instmem.mif> <instmem_size in byte> <datamem.mif> <datamem_size in byte> <xxx.S> [xxx.S] ..." % (sys.argv[0])
         sys.exit(1)
-    instmif = MifFile(sys.argv[1])
-    instmif.init(int(sys.argv[2]), 1)
-    datamif = MifFile(sys.argv[3])
-    datamif.init(int(sys.argv[4]), 4)
+    if sys.argv[1] != '-':
+        instmif = MifFile(sys.argv[1])
+        instmif.init(int(sys.argv[2]), 1)
+    if sys.argv[3] != '-':
+        datamif = MifFile(sys.argv[3])
+        datamif.init(int(sys.argv[4]), 4)
     assembler = Assembler()
     for filename in sys.argv[5:]:
         assembler.compile(filename)
     assembler.relocate()
-    assembler.instmem.fill(instmif.memory)
-    assembler.datamem.fill(datamif.memory)
-    instmif.write_back()
-    datamif.write_back()
+    if instmif != None:
+        assembler.instmem.fill(instmif.memory)
+        instmif.write_back()
+    if datamif != None:
+        assembler.datamem.fill(datamif.memory)
+        datamif.write_back()
 
 if __name__ == "__main__":
     main()
